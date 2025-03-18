011e69ccf9e82d4599e7b3ff28162a15321b027b
Lisiane Hoffmeister <lisiane.h67@gmail.com>
2025-03-17 21:42:31 -0300

    Exercicio inicial aula 01

---
+++ aula01/Camisa.py
@@ -0,0 +1,12 @@
+class Camisa:
+
+    def __init__(self, tamanho = None, cor = "Amarelo"):
+        self.tamanho = tamanho
+        self.cor = cor
+        self.preco = 0
+
+##Opções para intanciar o Objeto quando temos cadastrado o valor default
+## a = Camisa("M", "Azul")
+## b = Camisa("G")
+## c = Camisa()
+## d = Camisa(cor = "Vermelho")
