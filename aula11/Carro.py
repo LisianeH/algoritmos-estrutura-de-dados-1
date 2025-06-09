6e2ea04340cd7e07270a9b63335de9733fee6a43
Lisiane Hoffmeister <lisiane.h67@gmail.com>
2025-06-09 20:37:30 -0300

    Aula 11

---
+++ aula11/Carro.py
@@ -0,0 +1,10 @@
+
+class Carro:
+
+    def __init__( self, modelo, ano, placa):
+        self.modelo = modelo
+        self.ano = ano
+        self.placa = placa
+        self.inicio = None
+        self.fim = None
+        self.prox = None
